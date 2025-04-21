58789945d8fe8f026be6b9c51089837cb9ccd779
anikannal <anikannal@gmail.com>
2025-04-22 01:48:09 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,8 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
+import React from "react";
+import { GameProvider } from "@/context/GameContext";
 import Home from "@/pages/home";
 import Game from "@/pages/game";
 
@@ -21,8 +23,12 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <Toaster />
-        <Router />
+        <GameProvider children={
+          <>
+            <Toaster />
+            <Router />
+          </>
+        } /> {/* Wrap the Router with GameProvider */}
       </TooltipProvider>
     </QueryClientProvider>
   );
